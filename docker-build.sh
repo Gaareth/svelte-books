d9b23b3fca98b3ceb05498c63b055206cce981f5
Gaareth <gareth04@outlook.de>
2024-11-09 16:42:54 +0100

    Add echo to build scripts

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,5 @@ set -e;
 
 npm run build && ./docker-build-local.sh;
 npm version patch --force;
+echo "> Uploading v$(jq -r .version package.json)"
 docker buildx build  --platform=linux/amd64 . -t ghcr.io/gaareth/svelte-books:$(jq -r .version package.json) -t ghcr.io/gaareth/svelte-books:latest --push;
