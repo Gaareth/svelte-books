90aebc32912ede15c4479e2b1c015b11595e4e85
Gaareth <gareth04@outlook.de>
2024-11-09 14:12:57 +0100

    bookcreation use optional date

--- src/routes/book/create/+server.ts
+++ src/routes/book/create/+server.ts
@@ -5,6 +5,17 @@ import { prisma } from "$lib/server/prisma";
 import { getBookApiData } from "../api/api.server";
 import { extractBookApiData, extractCategories } from "$lib/server/db/utils";
 
+const optionalDatetimeSchema = z.object({  
+  day: z.number().int().min(1).max(31).optional(), 
+  month: z.number().int().min(1).max(12).optional(), 
+  year: z.number().int().min(0).optional(),
+
+  hour: z.number().int().min(0).max(23).optional(),
+  minute: z.number().int().min(0).max(59).optional(),
+
+  timezone: z.number().min(0).optional(), // Optional timezoneoffset in minutes
+});
+
 const createSchema = z.object({
   name: z.string().trim().min(1),
   author: z.string().trim().min(1),
@@ -12,7 +23,8 @@ const createSchema = z.object({
   words_per_page: z.number().optional(),
   listName: z.string().trim().min(1),
   volumeId: z.string().trim().optional(),
-  read_now: z.boolean().optional(),
+  dateStarted: optionalDatetimeSchema.optional(),
+  dateFinished: optionalDatetimeSchema.optional(),
 });
 
 export async function POST(req: RequestEvent) {
@@ -32,7 +44,8 @@ export async function POST(req: RequestEvent) {
       words_per_page,
       listName,
       volumeId,
-      read_now,
+      dateStarted,
+      dateFinished
     } = result.data;
 
     const book_exist = await prisma.book.findFirst({ where: { name } });
@@ -49,8 +62,12 @@ export async function POST(req: RequestEvent) {
       data: {
         name,
         author,
-        monthRead: read_now ? now.getMonth() + 1 : undefined,
-        yearRead: read_now ? now.getFullYear() : undefined,
+        dateStarted: {
+          create: dateStarted
+        },
+        dateFinished: {
+          create: dateFinished
+        },
         bookList: {
           connectOrCreate: {
             where: {
@@ -72,58 +89,62 @@ export async function POST(req: RequestEvent) {
       },
     });
 
-    if (volumeId !== undefined) {
-      const apiData = await getBookApiData(volumeId);
-      console.log(apiData);
-
-      const extractedData = extractBookApiData(apiData);
-
-      const bookApiData = await prisma.bookApiData.findUnique({
-        where: { id: apiData.id },
-      });
-
-      if (bookApiData !== null) {
-        await prisma.bookApiData.update({
-          where: { id: apiData.id },
-          data: extractedData,
-        });
-        // message = `Updated api data (${apiData.id}) from ${bookApiData?.title} to ${name}`;
-      }
-
-      await prisma.book.update({
-        where: { name },
-        data: {
-          bookApiData: {
-            connectOrCreate: {
-              where: { id: apiData.id },
-              create: extractedData,
-            },
-          },
-        },
-      });
-
-      const categories = extractCategories(apiData);
-
-      console.log(categories);
-
-      for (const category_str of categories) {
-        const category = await prisma.bookCategory.upsert({
-          where: { name: category_str },
-          update: {
-            name: category_str,
-            books: { connect: { id: apiData.id } },
-          },
-          create: {
-            name: category_str,
-            books: { connect: { id: apiData.id } },
-          },
-        });
-
-        console.log(category);
-      }
-    }
+    await addApiData(volumeId, name);
 
     return json({ success: true });
   }
   error(400);
 }
+async function addApiData(volumeId: string | undefined, name: string) {
+  if (volumeId !== undefined) {
+    const apiData = await getBookApiData(volumeId);
+    console.log(apiData);
+
+    const extractedData = extractBookApiData(apiData);
+
+    const bookApiData = await prisma.bookApiData.findUnique({
+      where: { id: apiData.id },
+    });
+
+    if (bookApiData !== null) {
+      await prisma.bookApiData.update({
+        where: { id: apiData.id },
+        data: extractedData,
+      });
+      // message = `Updated api data (${apiData.id}) from ${bookApiData?.title} to ${name}`;
+    }
+
+    await prisma.book.update({
+      where: { name },
+      data: {
+        bookApiData: {
+          connectOrCreate: {
+            where: { id: apiData.id },
+            create: extractedData,
+          },
+        },
+      },
+    });
+
+    const categories = extractCategories(apiData);
+
+    console.log(categories);
+
+    for (const category_str of categories) {
+      const category = await prisma.bookCategory.upsert({
+        where: { name: category_str },
+        update: {
+          name: category_str,
+          books: { connect: { id: apiData.id } },
+        },
+        create: {
+          name: category_str,
+          books: { connect: { id: apiData.id } },
+        },
+      });
+
+      console.log(category);
+    }
+  }
+}
+
