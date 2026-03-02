28a5975d7687d99e15703617f00ad08970ab437b
Gaareth <gareth04@outlook.de>
2026-03-02 23:36:11 +0100

    fix: return registration code not url

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -28,7 +28,7 @@
   async function copyToClipboard(code: string) {
     if (navigator.clipboard && navigator.clipboard.writeText) {
       try {
-        await navigator.clipboard.writeText("/register/" + code);
+        await navigator.clipboard.writeText(code);
         toast.success("Copied registration link to clipboard");
       } catch (err) {
         console.error("Failed to copy text:", err);
