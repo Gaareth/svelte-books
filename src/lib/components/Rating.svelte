9d35799cda8803f1789bb8cab9056c5509d39dd2
Gaareth <gareth04@outlook.de>
2026-08-16 00:19:26 +0200

    fix: ensure unique keys in rating component and adjust icon sizes

--- src/lib/components/Rating.svelte
+++ src/lib/components/Rating.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import StarIcon from "../icons/Star/StarIcon.svelte";
     import StarOutlineIcon from "../icons/Star/StarOutlineIcon.svelte";
+    import { v4 as uuidv4 } from "uuid";
+
 
     interface Props {
         rating: number | undefined | null;
@@ -28,17 +30,19 @@
             rating = i;
         }
     };
+
+    const uuid = uuidv4();
 </script>
 
 <div class="flex flex-row flex-wrap">
-    {#each Array(rating_max) as _, i}
+    {#each Array(rating_max) as _, i (uuid + String(i))}
         {#if i + 1 <= (rating ?? 0)}
             <button
                 class="icon"
                 disabled={!editable}
                 onclick={() => decreaseRating(i + 1)}
                 type="button">
-                <StarIcon />
+                <StarIcon class="w-8 h-8"/>
             </button>
         {:else}
             <button
@@ -46,15 +50,8 @@
                 disabled={!editable}
                 onclick={() => increaseRating(i + 1)}
                 type="button">
-                <StarOutlineIcon />
+                <StarOutlineIcon class="w-8 h-8"/>
             </button>
         {/if}
     {/each}
 </div>
-
-<style>
-    .icon {
-        width: 32px;
-        height: 32px;
-    }
-</style>
